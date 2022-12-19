37adf307f342348c29d252aaf82d45a1d9170b5a
Oscar BOUTARFA <oboutarf@e1r4p17.clusters.42paris.fr>
2022-12-19 23:56:50 +0100

    { 19/12/2022 || 23:56 }

---
+++ philo/incld/philosophers.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oboutarf <oboutarf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/18 00:47:55 by oboutarf          #+#    #+#             */
+/*   Updated: 2022/12/19 10:46:35 by oboutarf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOPSOPHERS_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+
+#endif
